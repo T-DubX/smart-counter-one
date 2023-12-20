493945a09f42e2f2afbf17140eb5fe7d5911edbe
Anton Brashkov <spone161@gmail.com>
2023-12-20 18:40:37 +0300

    refactoring of work with localstorage

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,47 +1,9 @@
 import './App.css'
 import {Counter} from "./components/counter/Counter.tsx";
 import {CounterSetting} from "./components/counterSetting/CounterSetting.tsx";
-import {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType} from "./store/store.tsx";
-import {setCounterAC} from "./store/counterReducer.tsx";
-import {setMaxValueAC, setStartValueAC} from "./store/settingsReducer.tsx";
 import styles from './components/counter/Counter.module.css'
 
 function App() {
-
-    const counter = useSelector<AppRootStateType, number>(state => state.counter.count)
-
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        const valueAsString = localStorage.getItem('counterValue')
-        const minCountAsString = localStorage.getItem('min')
-        const maxCountAsString = localStorage.getItem('max')
-
-        if (valueAsString) {
-            let newValue = JSON.parse(valueAsString)
-
-            dispatch(setCounterAC(newValue))
-        }
-
-        if (minCountAsString) {
-            let newValue = JSON.parse(minCountAsString)
-            dispatch(setStartValueAC(newValue))
-        }
-
-
-        if (maxCountAsString) {
-            let newValue = JSON.parse(maxCountAsString)
-            dispatch(setMaxValueAC(newValue))
-        }
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem('counterValue', JSON.stringify(counter))
-    }, [counter])
-
-
 
     return (
         <div className={styles.container}>
--- vite-project/src/store/store.tsx
+++ vite-project/src/store/store.tsx
@@ -1,6 +1,8 @@
-import {combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {counterReducer} from "./counterReducer.tsx";
 import {settingsReducer} from "./settingsReducer.tsx";
+import {loadState, saveState} from "./localstorage/localstorage-utils.ts";
+import {thunk} from "redux-thunk";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -8,9 +10,14 @@ const rootReducer = combineReducers({
     counter: counterReducer,
     setting: settingsReducer
 })
+const persistedState = loadState();
 // непосредственно создаём store
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, persistedState, applyMiddleware(thunk));
 // определить автоматически тип всего объекта состояния
+
+store.subscribe(() => {
+    saveState(store.getState())
+})
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
